ff6f59df7bf0bfac40fb2048d66ae41accb4f43a
mmorenorolon <mmorenorolon@ucsb.edu>
2025-08-19 11:46:43 -0700

    joins

--- data-wrangling-practice.R
+++ data-wrangling-practice.R
@@ -32,3 +32,53 @@ penguins_sub2 <- penguins %>%
   summarise(mean_length = mean(flipper_length_mm),
             sd_length = sd(flipper_length_mm),
             sample_size = n())
+
+#Interactive Session Animals and Sites dataframes
+animals <- data.frame(
+  stringsAsFactors = FALSE,
+          location = c("lagoon", "bluff", "creek", "oaks", "bluff"),
+           species = c("bobcat", "coyote", "fox", "squirrel", "bobcat"),
+          maturity = c("adult", "juvenile", "adult", "juvenile", "adult")
+)
+
+sites <- data.frame(
+  stringsAsFactors = FALSE,
+          location = c("beach", "lagoon", "bluff", "oaks"),
+    full_site_name = c("Goleta Beach","UCSB Lagoon",
+                       "Ellwood Mesa","Fremont Campground"),
+      jurisdiction = c("SB City", "UCSB", "SB City", "USFS")
+)
+
+#MUTATING JOINS
+
+#full_join() example
+  #keeps all rows and adds all columns
+
+library(dplyr)
+full_join_example <- full_join(animals, sites)
+
+#left_join()
+
+left_join_example <- left_join(animals, sites)
+
+#right_join()
+
+right_join_example <- right_join(animals, sites)
+
+#inner_join()
+
+inner_join_example <- inner_join(animals, sites)
+
+#FILTERING JOINS
+
+#semi_join()
+
+semi_join_example <- semi_join(animals, sites)
+
+animals %>% 
+  filter(location %in% sites$location)
+
+#anti_join()
+
+anti_join_example <- anti_join(animals, sites)
+
